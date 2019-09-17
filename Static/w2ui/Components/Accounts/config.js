8a2572a8fe507bc9ac7dfa811855381ea0cf41a1
Andrey Makrushin <mag.ondrei@gmail.com>
2019-09-17 11:30:00 +0300

    Добавил описание некоторых пунктов меню

--- Static/w2ui/Components/Accounts/config.js
+++ Static/w2ui/Components/Accounts/config.js
@@ -1,7 +1,6 @@
 layout_account = {
   name: 'layout_account',
   padding: 0,
-  //style: 'height: 100%; position: relative; width:100%;',
   panels: [
     { type: 'main', size: '30%',  content: 'main' },
     { type: 'preview', size:'70%', content: '7'}
--- Static/w2ui/Components/sidebar.js
+++ Static/w2ui/Components/sidebar.js
@@ -1,14 +1,19 @@
 sidebar_config = {
         name: 'sidebar',
         flatButton: true,
-        topHTML    : '<div style="height: 30px;"></div>',
-        bottomHTML : '<div style="background-color: #eee; padding: 10px 5px; border-top: 1px solid silver">Куджима</div>',
+        topHTML    : '<div style="height: 30px;" id="helpsidebar"></div>',
+        bottomHTML : '<div style="background-color: #f7f7f7; padding: 8px; border-top: 1px solid #63acdd; color: #539ed7;"><i style="font-size: xx-large;float: inline-end;" class="fa fa-info-circle" aria-hidden="true"></i> <div id="bottomHTML"></div></div>',
         nodes: [
           { id: 'tools', text: 'Инструменты', img: 'icon-folder', expanded: true, group: true,
             nodes: [
-              { id: 'trash', text: 'Не сортированные', icon: 'fa fa-trash-o', count: 5 },
-              { id: 'future', text: 'Скоро', icon: 'fa fa-calendar-check-o', count: 1 },
-              { id: 'confirm', text: 'Подтверждение', icon: 'fa fa-check-square', count: 256 }
+              { id: 'trash', text: 'Не сортированные', icon: 'fa fa-trash-o', count: 5,
+                comment:"Транзакции зарегистрированы через мобильную форму, имеют не заполненные поля."},
+
+              { id: 'future', text: 'Скоро', icon: 'fa fa-calendar-check-o', count: 1
+                ,comment:"Не подтвержденные транзакции, дата выполнения которых запланирована на ближайшие 7 дней."},
+
+              { id: 'confirm', text: 'Подтверждение', icon: 'fa fa-check-square', count: 256
+                ,comment:"Не подтвержденные транзакции у которых дата выполнения уже прошла."}
             ]
           },
 
@@ -18,7 +23,7 @@ sidebar_config = {
                        { id: 'cost_item', text: 'Статьи', icon: 'fa fa-pie-chart' },]
                      }
           ],
-            onFlat: function (event) {
+        onFlat: function (event) {
               if (event.goFlat) {
                 w2ui.base_layout.get('left').size = 40;
               } else {
@@ -27,9 +32,8 @@ sidebar_config = {
               w2ui.base_layout.resize();
             },
 
-
         onClick: function(event) {
-
+          console.log(event);
             // Отображаем меню
 
             if (event.target == 'account') {
@@ -41,6 +45,11 @@ sidebar_config = {
             if (event.target == 'trash') {
               w2ui.base_layout.content('main', w2ui.transact_grid);
             }
+
+            if ('comment' in event.object)
+              {$("#bottomHTML").html(event.object.comment) }
+            else {$("#bottomHTML").html(event.object.text);}
+
 
         }
     }
