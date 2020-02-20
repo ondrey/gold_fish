15b87a36da33fc3a5e276156a77c3a5b5bda5e37
Andrey Makrushin <mag.ondrei@gmail.com>
2020-02-20 11:20:12 +0300

    Парвка отображения счетов

--- Static/w2ui/Components/Accounts/config.js
+++ Static/w2ui/Components/Accounts/config.js
@@ -6,12 +6,43 @@ layout_account = {
     { type: 'preview', size:'70%', content: '7', resizable: true}
   ]
 }
+
+add_form_account = { 
+    header: 'Edit Record',
+    name: 'add_form_account',
+    fields: [
+        { name: 'recid', type: 'text', html: { caption: 'ID', attr: 'size="10" readonly' } },
+        { name: 'fname', type: 'text', required: true, html: { caption: 'First Name', attr: 'size="40" maxlength="40"' } },
+        { name: 'lname', type: 'text', required: true, html: { caption: 'Last Name', attr: 'size="40" maxlength="40"' } },
+        { name: 'email', type: 'email', html: { caption: 'Email', attr: 'size="30"' } },
+        { name: 'sdate', type: 'date', html: { caption: 'Date', attr: 'size="10"' } }
+    ],
+    actions: {
+        Reset: function () {
+            this.clear();
+        },
+        Save: function () {
+            var errors = this.validate();
+            if (errors.length > 0) return;
+            if (this.recid == 0) {
+                w2ui.grid.add($.extend(true, { recid: w2ui.grid.records.length + 1 }, this.record));
+                w2ui.grid.selectNone();
+                this.clear();
+            } else {
+                w2ui.grid.set(this.recid, this.record);
+                w2ui.grid.selectNone();
+                this.clear();
+            }
+        }
+    }
+}
+
 config_accounts = {
         name: 'config_accounts',
         url  : {
             get    : '/acc/get_account_list',
             remove : '',
-            save   : ''
+            save   : '/acc/add_account'
         },
         method: 'POST',
         show: {
@@ -22,14 +53,40 @@ config_accounts = {
             toolbarEdit: true,
             columnHeaders: true
         },
-        selectType : 'row',
-        columns: [
-            { field: 'title_acc', caption: 'Наименование счета/статьи', size: '60%' },
+        // items: [
+        //     { id: 'add', type: 'button', caption: 'Add Record', icon: 'w2ui-icon-plus' }
+        // ],
 
-            { field: 'in', caption: 'Входящий', size: '10%' },
-            { field: 'income', caption: 'Приход', size: '10%' },
-            { field: 'cost', caption: 'Расход', size: '10%' },
-            { field: 'out', caption: 'Исходящий', size: '10%' },
-            { field: 'name_user_owner', caption: 'Владелец', size: '10%'},
-        ]
+        columns: [
+            { field: 'title_acc', caption: 'Наименование счета/статьи', size: '330px' },
+
+            { field: 'in', caption: 'Входящий', size: '120px' },
+            { field: 'income', caption: 'Приход', size: '120px' },
+            { field: 'cost', caption: 'Расход', size: '120px' },
+            { field: 'out', caption: 'Исходящий', size: '120px' },
+            { field: 'name_user_owner', caption: 'Владелец', size: '100%'},
+        ],
+        
+        onAdd: function(event) {
+            console.log(event)
+
+            w2popup.open({
+                title   : 'Редактирование',
+                width   : 900,
+                height  : 600,
+                showMax : true,
+                body    : '<div id="smain" style="position: absolute; left: 5px; top: 5px; right: 5px; bottom: 5px;"></div>',
+                onOpen  : function (event) {
+                    event.onComplete = function () {
+                        $('#smain').w2form(w2ui.add_form_account)
+                    };
+                },
+                onToggle: function (event) { 
+                    event.onComplete = function () {
+                        w2ui.layout.resize();
+                    }
+                }
+            });
+
+        }  
     }
--- api/lk/Account.py
+++ api/lk/Account.py
@@ -30,14 +30,12 @@ class Account(ObjectAPI, ObjectDb):
                 select 
                 acc.id_acc,
                 acc.id_par_acc,
-                acc.addate_acc,               
-                acc.id_type_acc,
-                acct.title_type_acc,                
+                acc.addate_acc,                          
                 acc.title_acc,                
                 acc.id_user_owner,
                 us.name_user,                
                 acc.is_public,                								
-                COALESCE(i.class_icon, acct.icon_type_acc) as class,                
+                i.class_icon,                
                 acc.color_acc,
                 acc.discription_acc
                 
@@ -47,8 +45,7 @@ class Account(ObjectAPI, ObjectDb):
                 on us.id_user = acc.id_user_owner
                 left join Icons as i 
                 on i.id_icon = acc.id_icon
-                left join AccountTypes as acct 
-                on acct.id_type_acc = acc.id_type_acc
+
             WHERE 
             
             (acc.id_user_owner = {0} or {2}) {1}
@@ -64,27 +61,36 @@ class Account(ObjectAPI, ObjectDb):
         records = []
         for i in cur.fetchall():
             ch = self.create_account_list(i[0])
-            w2ui = {'style': 'color:{0}'.format(i[10])}
+            w2ui = {}
             if len(ch) > 0:
                 w2ui.update({'children': ch})
+            if i[8]:
+                w2ui.update({'style': 'color:{0}'.format(i[8])})
 
             records.append(dict(
                 recid=i[0],
-                parent_=i[1],
-                addate_acc=i[2],
-                id_type_acc=i[3],
-                title_type_acc=i[4],
-                title_acc=i[5],
-                id_user_owner=i[6],
-                name_user_owner=i[7],
-                is_public=i[8],
-                icon_class=i[9],
-                discription_acc=i[11],
-                #w2ui=w2ui
+                id_par_acc=i[1],
+                addate_acc=i[2],                          
+                title_acc=u"{1} <i style='font-size: large; float:right;' class='fa {0}'></i>".format(i[7], i[3]),
+                id_user_owner=i[4],
+                name_user_owner=i[5],
+                is_public=i[6],                								
+                class_icon=i[7],
+                color_acc=i[8],
+                discription_acc=i[9],
+                w2ui=w2ui
             ))
 
         return records
 
     @isauth
     def api_get_account_list(self):
-        return jsonify(self.create_account_list())
+        res = self.create_account_list()
+        return jsonify({
+            'total': len(res),
+            'records': res
+        })
+
+    @isauth
+    def api_add_account(self):
+        pass
